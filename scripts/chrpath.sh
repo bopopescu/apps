80c8dfdf1c98e166632dc6ecb33269f92bbf9d81
Hongjiu Zhang <zhanghj@umich.edu>
2016-03-16 17:58:30 -0400

    Add chrpath
    chrpath is to remove library linkage from binaries. This is to make sure
    that library switching or upgrading takes effect. However, this would
    break programs if different library versions are incompatible.
    
    Signed-off-by: Hongjiu Zhang <zhanghj@umich.edu>

---
+++ scripts/chrpath.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export MODAPP_PATH="$(dirname $(readlink -e "${BASH_SOURCE[0]}"))"
+pushd "$MODAPP_PATH/../images"
+module load chrpath
+find . -exec sh -c "file {} | grep ELF | grep -v '^\./\(jdk\|anaconda\)/' && chmod +w {} && chrpath -d {} && echo {}" \;
+module unload chrpath
+popd
