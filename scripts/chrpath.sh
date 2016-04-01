5360fa74d2e4bd4aac381c0a290a7ddda32a0cf2
Hongjiu Zhang <zhanghj@umich.edu>
2016-04-01 15:54:37 -0400

    Add an optional parameter for chrpath.sh
    Iterating over all images for chrpath is slow and often unnecessary. Add
    an optional parameter to help speed up the process.
    
    Signed-off-by: Hongjiu Zhang <zhanghj@umich.edu>

--- scripts/chrpath.sh
+++ scripts/chrpath.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export MODAPP_PATH="$(dirname $(readlink -e "${BASH_SOURCE[0]}"))"
-pushd "$MODAPP_PATH/../images"
+pushd "$MODAPP_PATH/../images/${1}"
 module load chrpath
 find . -exec sh -c "file {} | grep ELF | grep -v '^\./\(jdk\|anaconda\)/' && chmod +w {} && chrpath -d {} && echo {}" \;
 module unload chrpath
